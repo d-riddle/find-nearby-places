5abc0de9311880634b3025e9c1d13500383e7cf2
Ankit Kumar Sahoo <akumarsa@visa.com>
2022-12-27 16:06:24 +0530

    deploy render trying..

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const cors=require("cors");
+// const cors=require("cors");
 const app=express();
 const path=require("path");
 
@@ -10,7 +10,12 @@ const placesRoute=require("./routes/places");
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/api/places",placesRoute);
 
